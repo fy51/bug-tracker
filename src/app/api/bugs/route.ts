4cd7e64bfecc19eba4f62a5fc55696f900915f18
fy51 <18986365477@189.cn>
2023-10-29 16:46:30 +0800

    Implement server-side POST data validation

--- src/app/api/bugs/route.ts
+++ src/app/api/bugs/route.ts
@@ -1,10 +1,15 @@
 import prisma from "@/lib/db";
+import { bugSchema } from "@/lib/validation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+  const body: unknown = await request.json();
+  const validation = bugSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 422 });
+  }
 
-  const { title, description } = body;
+  const { title, description } = validation.data;
   const bug = await prisma.bug.create({
     data: { title, description },
   });
