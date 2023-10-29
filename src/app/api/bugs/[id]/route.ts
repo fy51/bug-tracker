b182102c8259cb14d7122f6d0b17abdcdbb81114
fy51 <18986365477@189.cn>
2023-10-29 19:08:57 +0800

    Build an API for updating bug

---
+++ src/app/api/bugs/[id]/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/lib/db";
+import { patchBugSchema } from "@/lib/validation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  // 1. Check request body
+  const body: unknown = await request.json();
+  const validation = patchBugSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 422 });
+  }
+
+  // 2. Check whether the id exists
+  const bug = await prisma.bug.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!bug) {
+    return NextResponse.json("Invalid bug", { status: 404 });
+  }
+
+  // 3. Update data
+  const { title, description } = validation.data;
+  const updatedBug = await prisma.bug.update({
+    where: {
+      id: bug.id,
+    },
+    data: {
+      title,
+      description,
+    },
+  });
+  return NextResponse.json(updatedBug);
+}
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -4,3 +4,12 @@ export const bugSchema = z.object({
   title: z.string().min(1, "Title is required").max(255),
   description: z.string().min(1, "Description is required.").max(255),
 });
+
+export const patchBugSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(255)
+    .optional(),
+});
