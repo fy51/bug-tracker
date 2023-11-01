2e42de33fb16163ba7be9ea9f6cc18d352e2298f
fy51 <18986365477@189.cn>
2023-11-01 09:26:15 +0800

    Build BugDetails component

--- src/app/bugs/detail/[id]/page.tsx
+++ src/app/bugs/detail/[id]/page.tsx
@@ -1,10 +1,21 @@
+import prisma from "@/lib/db";
 import { Box, Button, Flex, Grid } from "@radix-ui/themes";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+import BugDetails from "./BugDetails";
 
-const BugDetailPage = ({ params }: { params: { id: string } }) => {
+const BugDetailPage = async ({ params }: { params: { id: string } }) => {
+  const bug = await prisma.bug.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!bug) notFound();
+
   return (
     <Grid columns="5" gap="5">
-      <Box className="col-span-4">BugDetail</Box>
+      <Box className="col-span-4">
+        <BugDetails bug={bug} />
+      </Box>
       <Box>
         <Flex direction="column" gap="3">
           <div>AssigneeSelect</div>
