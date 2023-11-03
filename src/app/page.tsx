daf5f4437af556d5c610fd1b307abbeda5cbfab5
fy51 <18986365477@189.cn>
2023-11-03 08:29:08 +0800

    Build Dashboard Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div>Home</div>;
+import prisma from "@/lib/db";
+import { Grid } from "@radix-ui/themes";
+
+export default async function Home() {
+  const open = await prisma.bug.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.bug.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.bug.count({ where: { status: "CLOSED" } });
+
+  return (
+    <Grid columns="2">
+      <div>
+        <div>BugSummary</div>
+        <div>BubChart</div>
+      </div>
+      <div>LatestBugs</div>
+    </Grid>
+  );
 }
